780c43ca6198ea230bd8ea434ba94342abc1ec21
Магомед-Эмин Исламов <mislamov@astralinux.ru>
2024-11-11 21:04:29 +0300

    Правки после ревью

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,6 @@
 export { LoginPage } from './login'
 export { SignUpPage } from './sign-up'
 export { AccountPage } from './account'
-export { ListPage } from './list'
 export { NavigatePage } from './navigate'
 export { Error404Page } from './error404'
 export { Error500Page } from './error500'
